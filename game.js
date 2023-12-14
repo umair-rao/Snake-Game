c5e8ac67dccbf3b76a1cc487e4d5ca23b4c4372b
Umair Rao <umairmuqeem@gmail.com>
2023-12-14 11:46:16 +0500

    Add update Snake Function

--- game.js
+++ game.js
@@ -1,5 +1,7 @@
+import { update as updateSnake, draw as drawSnake, SNAKE_SPEED } from "./snake.js";
+
 let lastRenderTime = 0;
-const SNAKE_SPEED = 2;
+const gameBoard = document.getElementById('game-board')
 
 function main(currentTime) {
     window.requestAnimationFrame(main)
@@ -7,15 +9,17 @@ function main(currentTime) {
 
     if(secondsSinceLastRender < 1 / SNAKE_SPEED) return
     lastRenderTime = currentTime;
-    console.log('Render')
+    update();
+    draw();
 }
 
 window.requestAnimationFrame(main)
 
 function update() {
-
+    updateSnake()
 }
 
 function draw() {
-    
+    gameBoard.innerHTML = ''
+    drawSnake(gameBoard)
 }
