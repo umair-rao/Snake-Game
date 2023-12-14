9ff94080840fad6e5de9cbb84f8933b63939cd61
Umair Rao <umairmuqeem@gmail.com>
2023-12-14 10:33:29 +0500

    Add Redner Time

---
+++ game.js
@@ -0,0 +1,21 @@
+let lastRenderTime = 0;
+const SNAKE_SPEED = 2;
+
+function main(currentTime) {
+    window.requestAnimationFrame(main)
+    const secondsSinceLastRender  = (currentTime - lastRenderTime) / 1000;
+
+    if(secondsSinceLastRender < 1 / SNAKE_SPEED) return
+    lastRenderTime = currentTime;
+    console.log('Render')
+}
+
+window.requestAnimationFrame(main)
+
+function update() {
+
+}
+
+function draw() {
+    
+}
